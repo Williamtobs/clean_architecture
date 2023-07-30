71743a11166dbc9cf060c7c795bb7fc172d3a096
Tobi myver <akeem.tobi@myver.co>
2023-07-30 13:01:21 +0100

    constant file

---
+++ lib/src/utils/constant/app_strings.dart
@@ -0,0 +1,6 @@
+//apps Title
+const String appTitle = 'Clean Architecture';
+
+//news api
+const String newsApiKey = '322474e494ef46f791365f86e1505992';
+const String newsApiUrl = 'https://newsapi.org/v2';
